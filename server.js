812a1e2b3710d9934f2d19454b09e243312b96ee
Radya <radya@gmx.com>
2016-11-27 21:05:01 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+
+app.use (function(req, res, next) {
+    var data='';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { 
+       data += chunk;
+    });
+
+    req.on('end', function() {
+        req.body = data;
+        next();
+    });
+});
+
+app.post('/*', function (req, res) {
+    var responText;
+    var responCode;
+    if (req.get('Content-Type') == 'application/json' && !!req.body) {
+        var url = require('url').parse(req.url.substr(1));
+        if (url.hostname != null) {
+            var options = {
+                host: url.hostname,
+                port: url.port || 80,
+                path: url.path,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST'
+            };
+            var request = require('http').request(options);
+            request.write(req.body);
+            request.end();
+
+            responCode = 200;
+            responText = 'Forwarded';
+        } else {
+            responCode = 401;
+            responText = 'Url is not valid';
+        }
+    } else {
+        responCode = 403;
+        responText = 'Access not allowed';
+    }
+
+    res.status(responCode).send(responText);
+});
+
+app.use(express.static(__dirname + '/static'));
+
+app.use(function (req, res, next) {
+    res.status(404).send('What are you looking for?');
+});
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.listen(port, ipaddress);
---
+++ static/assets/main.js
@@ -0,0 +1,47 @@
+var token = document.getElementById('token');
+var url = document.getElementById('url');
+var submitBtn = document.getElementById('submit');
+
+token.onkeyup = function() {
+  if (token.value.length > 20) {
+    submitBtn.disabled = false;
+    url.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+    url.disabled = true;
+  }
+}
+
+url.onkeyup = function() {
+  submitBtn.disabled = false;
+  if (url.value.length > 0) {
+    if (!url.value.match(/^https?:\/\/([a-z0-9\.\-]+)(\.[a-z0-9\-]+)(\/.*)?$/i)) {
+      submitBtn.disabled = true;
+    }
+  }
+}
+
+document.getElementById('setwebhook').onsubmit = function() {
+  var result = document.getElementById('result');
+  result.style.display = 'block';
+  result.innerHTML  = "Please wait..";
+  var hook = url.value;
+  if (hook.substr(0, 7) == 'http://') {
+    if (window.location.protocol != 'https:') {
+      hook = "https://setwebhook.ga/"+hook;
+    } else {
+      hook = "https://"+window.location.host+"/"+hook;
+    }
+  } 
+  var xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    if (xhttp.status == 404) {
+      result.innerHTML = 'Invalid bot token';
+    } else {
+      var json = JSON.parse(this.responseText);
+      result.innerHTML  = json.description;
+    }
+  };
+  xhttp.open('GET', "https://api.telegram.org/bot"+token.value+"/setWebhook?url="+hook, true);
+  xhttp.send();
+}
